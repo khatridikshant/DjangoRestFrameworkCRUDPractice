eb813ddc536d92b8b1e4bc3bcf3d42a85c4a077c
Dikshant Khatri <khatridikshant@outlook.com>
2024-09-25 20:11:27 +0545

    URL for POST endpoint

--- newproject/api/urls.py
+++ newproject/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_fakeuser
+from .views import get_fakeuser, create_user
 
 urlpatterns = [
     path('users/', get_fakeuser, name='get_fakeuser')
+    path('users/create', create_user, name="Create User" )
 ]
