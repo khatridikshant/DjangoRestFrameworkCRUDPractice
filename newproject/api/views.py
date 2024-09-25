5802874b81cae6af4969da7dd6d8832266aadf66
Dikshant Khatri <khatridikshant@outlook.com>
2024-09-25 19:57:37 +0545

    Simple View For Now

--- newproject/api/views.py
+++ newproject/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializer import UserSerializer
 
 # Create your views here.
+
+@api_view(["GET"])
+def get_fakeuser(request):
+    return Response(UserSerializer({"name": "FakeName", "age" : 40}))
