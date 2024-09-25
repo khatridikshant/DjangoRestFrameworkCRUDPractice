107f41e4b4ad084f10bd5f68cc52f9eb886e536f
Dikshant Khatri <khatridikshant@outlook.com>
2024-09-25 20:09:13 +0545

    POST logic for new user

--- newproject/api/views.py
+++ newproject/api/views.py
@@ -9,4 +9,13 @@ from .serializer import UserSerializer
 
 @api_view(["GET"])
 def get_fakeuser(request):
-    return Response(UserSerializer({"name": "FakeName", "age" : 40}))
+    return Response(UserSerializer({"name": "FakeName", "age" : 40}).data)
+
+@api_view(["POST"])
+def create_user(request):
+    serializer = UserSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
