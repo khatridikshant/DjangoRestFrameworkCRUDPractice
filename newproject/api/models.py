f6cdb8a85329ca92b2b3881aec3bf5560bc0214c
Dikshant Khatri <khatridikshant@outlook.com>
2024-09-25 19:44:31 +0545

    Model Created

--- newproject/api/models.py
+++ newproject/api/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    age = models.IntegerField()
+    name = models.CharField(max_length= 115)
+
+    def __str__(self) -> str:
+        return self.name
     
